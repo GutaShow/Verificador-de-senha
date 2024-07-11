7dd8fcf5c8f34fad8bef63799d8e9a907722cbee
GutaShow <108829708+GutaShow@users.noreply.github.com>
2024-07-11 09:53:44 -0300

    1.0

---
+++ Verificador de senha (com While e sem While)/js.js
@@ -0,0 +1,37 @@
+//-------------------------------Exercicio 1-----------------------------//
+function verify(){
+    let sen1, sen2, sen3, sen4, sen5, senh1, senh2, senh3, senh4, senh5
+
+    sen1 = window.prompt('Digite sua senha');
+    sen2 = window.prompt('Digite sua senha novamente');
+    sen3 = window.prompt('Digite sua senha novamente');
+    sen4 = window.prompt('Digite sua senha novamente');
+    sen5 = window.prompt('Digite sua senha novamente');
+    switch (sen1, sen2, sen3, sen4, sen5){
+        case '1234':
+            document.getElementById("res").innerHTML =
+            "sua senha esta correta"
+        break;
+        default:  
+        document.getElementById("res").innerHTML =
+        "sua senha esta incorreta"  
+    }
+}
+function verify2(){
+    let sen1, sen2
+
+    sen1 = window.prompt('Digite sua senha');
+
+    do {
+        sen2 = window.prompt('Digite sua senha novamente');
+    }while(sen1 != sen2){
+        
+    }
+}
+//----------------------------Exercicio 2----------------------------//
+function ordenar1(){
+    let 
+
+
+
+}
